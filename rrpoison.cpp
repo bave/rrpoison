ebf9c670a7f2ee08b4d8b20f8ea0b6cbfaedaf4e
bave <inoue.tomoya@gmail.com>
2014-07-28 00:42:39 +0900

    update

--- rrpoison.cpp
+++ rrpoison.cpp
@@ -257,20 +257,20 @@ main(int argc, char** argv)
 void usage(char* prog_name)
 {
     printf("%s\n", prog_name);
-    printf("	Must..\n");
-    printf("	-s [source address]\n");
-    printf("	-d [destination address]\n");
-    printf("	-a [ans address] \n");
-    printf("	-r [req name]\n");
-    printf("	Option..");
-    printf("	-c [count number]  : 0 is loop   (default:0)\n");
-    printf("	-x [target port]   : 0 is random (default:0)\n");
-    printf("	-y [target dns_is] : 0 is random (default:0)\n");
-    printf("	-v : verbose mode (default:disable)\n");
+    printf("  Must..\n");
+    printf("    -s [source address]\n");
+    printf("    -d [destination address]\n");
+    printf("    -a [ans address] \n");
+    printf("    -r [req name]\n");
+    printf("  Option..\n");
+    printf("    -c [count number]  : 0 is loop   (default:0)\n");
+    printf("    -x [target port]   : 0 is random (default:0)\n");
+    printf("    -y [target dns_is] : 0 is random (default:0)\n");
+    printf("    -v : verbose mode (default:disable)\n");
     printf("\n");
     printf("If you want to spoof the source address,\n");
     printf("you set a same option (-s) alias address to the sending IF.\n");
-    printf("(Example: ifconfig lo0 alias x.x.x.x.)\n");
+    printf("(Example: ifconfig lo0 alias x.x.x.x/32)\n");
     return;
 }
 
